87aaf4927e05ec446a42dfc218e1c12863e91367
黒髪零 <104833210+KRKMZERO@users.noreply.github.com>
2023-03-11 20:57:29 +0900

    Add files via upload

---
+++ mods/scripts/combosystem.lua
@@ -0,0 +1,32 @@
+function goodNoteHit()
+	
+	combo = getProperty('combo')
+
+	if combo >= 10 then
+		addScore(10)
+	end
+	if combo >= 50 then
+		addScore(10)
+	end
+	if combo >= 100 then
+		addScore(10)
+	end
+	if combo >= 150 then
+		addScore(10)
+	end
+	if combo >= 200 then
+		addScore(10)
+	end
+	if combo >= 250 then
+		addScore(10)
+	end
+	if combo >= 300 then
+		addScore(10)
+	end
+	if combo >= 350 then
+		addScore(10)
+	end
+	if combo >= 400 then
+		addScore(10)
+	end
+end
